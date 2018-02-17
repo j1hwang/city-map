c87a685d2e98aeaa86a44931ab7735ee9c25afc6
Jaewon Hwang <enav247@gmail.com>
2018-02-17 23:23:19 +0900

    Make list view

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Responsive parts
 const body = $('body');
 const hamburger = $('#hamburger');
 const nav = $('#nav');
@@ -13,27 +14,51 @@ hamburger.click(function(e) {
 	} else {
 		(hamburger.position().left > body.width()*0.1)
 			? hamburger.css('left', '2%')
-			: hamburger.css('left', '80%');
+			: hamburger.css('left', '70%');
 	}
 	e.stopPropagation;
 });
 
 body.click(function(e) {
-	if(!($(e.target).is('#hamburger') || $(e.target).is('#nav'))){
-		if(body.width() < 1025) {
+	if(!($(e.target).is('#hamburger') || $(e.target).is('#nav') || $(e.target).is('li'))){
+		if(body.width() < 1008) {
 			nav.hide();
 			title.show();
 			hamburger.css('left', '2%');
 		}
 	}
 });
+// Responsive parts (end)
+
+let map;
+let markers = [];
+let locations = [
+	{title: 'Disneyland', location: {lat: 35.6329007, lng: 139.8782003}}, // attraction
+	{title: 'Tokyo Tower', location: {lat: 35.6585848, lng: 139.7432389}},
+	{title: 'Odaiba Statue of Liberty', location: {lat: 35.6298089, lng: 139.7618991}},
+	{title: 'City Hall Tower', location: {lat: 35.6892506, lng: 139.6896613}},
+
+	{title: 'Gonpachi', location: {lat: 35.6572753, lng: 139.7045559}}, // restaurant
+	{title: 'Tsujihan', location: {lat: 35.6651728, lng: 139.7434675}},
+	{title: 'Isen Honten', location: {lat: 35.6615181, lng: 139.6863567}},
+
+	{title: 'Sinjuku Gyoen', location: {lat: 35.6851806, lng: 139.7078577}}, // park
+	{title: 'Yoyogi Park', location: {lat: 35.6717403, lng: 139.6927507}},
+	{title: 'Ueno Park', location: {lat: 35.666489, lng: 139.7349774}},
+
+	{title: 'Senso-ji', location: {lat: 35.7147689, lng: 139.7947563}}, // temple
+	{title: 'Meiji Shrine', location: {lat: 35.6764019, lng: 139.6971319}}, 
+
+	{title: 'Tsukiji Market', location: {lat: 35.6654904, lng: 139.7684728}}, // shopping
+	{title: 'Harajuku Street', location: {lat: 35.6711042, lng: 139.7024456}},
+	{title: 'Roppongi Hills', location: {lat: 35.6604681, lng: 139.7270547}},
+];
 
 
-var map;
 // Function to initialize the map within the map div
 function initMap() {
 
-	var styledMapType = new google.maps.StyledMapType(
+	const styledMapType = new google.maps.StyledMapType(
 		[
 			{elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
 			{elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
@@ -148,32 +173,91 @@ function initMap() {
 	);
 
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 37.566535, lng: 126.97796919999996},
+		center: {lat: 35.6894875, lng: 139.69170639999993},
 		zoom: 14,
-		mapTypeControlOptions: {
-			mapTypeIds: ['']
-			//'roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
-		}
+		mapTypeControl: false
 	});
 
 	map.mapTypes.set('styled_map', styledMapType);
 	map.setMapTypeId('styled_map');
 
-	// Create a single latLng literal object.
-	var singleLatLng = {lat: 37.566535, lng: 126.97796919999996};
+	
+    let largeInfoWindow = new google.maps.InfoWindow();
+    let bounds = new google.maps.LatLngBounds();
 
-	marker = new google.maps.Marker ({
-		map: map,
-		position: singleLatLng,
-		title: 'i am here!!',
-		animation: google.maps.Animation.DROP,
-	});
+    for(let i = 0; i < locations.length; i++) {
+    	let position = locations[i].location;
+    	let title = locations[i].title;
+    	let marker = new google.maps.Marker({
+    		position: position,
+    		title: title,
+    		//animation: google.maps.Animation.DROP,
+    		id: i
+    	});
 
-	var infowindow = new google.maps.InfoWindow();
+    	markers.push(marker);
+    	marker.addListener('click', function() {
+    		populateInfoWindow(this, largeInfoWindow);
+    	});
+    }
+    
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
 
-	marker.addListener('click', function() {
-		infowindow.marker = this;
-		infowindow.setContent('<div>' + this.title + '</div>');
-		infowindow.open(map, this);
-	});
+    function populateInfoWindow(marker, infowindow) {
+    	if(infowindow.marker != marker) {
+    		infowindow.marker = marker;
+    		infowindow.setContent('<div>' + marker.title + '</div>');
+          	infowindow.open(map, marker);
+        
+          	// Make sure the marker property is cleared if the infowindow is closed.
+          	infowindow.addListener('closeclick', function() {
+            	infowindow.marker = null;
+        	});
+    	}
+    }
+
+    // function hideListings() {
+    //     for (var i = 0; i < markers.length; i++) {
+    //     	markers[i].setMap(null);
+    //     }
+    // }
 }
+
+
+let ViewModel = function() {
+	let self = this;
+
+	this.locationList = ko.observableArray([]);
+
+	locations.forEach(function(location) {
+		self.locationList.push(new Location(location));
+	});
+
+	this.currentLocation = ko.observable(self.locationList()[0]);
+
+	self.setLocation = function(clickedLocation) {
+
+		self.currentLocation = clickedLocation;
+		for (var i = 0; i < markers.length; i++) {
+        	markers[i].setMap(null);
+        }
+        setTimeout(function() {
+        	initMap();
+        },500);
+        console.log(self.currentLocation);
+		console.log(self.currentLocation.title());
+		console.log(self.currentLocation.location());
+	}
+}
+
+
+let Location = function(data) {
+	this.title = ko.observable(data.title);
+	this.location = ko.observable(data.location);
+}
+
+ko.applyBindings(new ViewModel());
